e1ebea31106dc4ae70c227523c38f043afd53e31
kosuyolueah <kosuyolueah.temizliktakip@gmail.com>
2025-11-12 10:09:46 +0300

    Update GoogleSheet.js

--- YatanHastaKlinikleriK.2B/GoogleSheet.js
+++ YatanHastaKlinikleriK.2B/GoogleSheet.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyziVarzXw-FoRzoGqRfg4u8_qP9EI9guv02CT84YHzmNmTePsiW4syKF85TadgxShk/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycby9APp3LSIZ6P3JOc2yJntZ9VQDFDCDgP5n9aKFLWbNTj6xZ9QENmRhcnp97zENDiJI/exec'
 
 const form = document.forms['contact-form']
 
